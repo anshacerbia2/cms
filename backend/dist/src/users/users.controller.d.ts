db97893b7190bf6d66510deff0df2cce5d6060d5
Ansha Cerbia <108260133+anshacerbia2@users.noreply.github.com>
2026-04-14 20:27:05 +0700

    chore: ignore and untrack backend dist folder to keep source control clean

--- backend/dist/src/users/users.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UsersService } from './users.service';
-import { PaginationQueryDto } from '../common/dto/pagination.dto';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    findAll(query: PaginationQueryDto): Promise<import("../common/interfaces/paginated-result.interface").PaginatedResult<any>>;
-}
